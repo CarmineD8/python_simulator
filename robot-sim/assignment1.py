cad0ec3502a5c344ddc3d287cd4e8e34b9f95040
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2022-03-31 23:31:09 +0000

    file updated

--- robot-sim/assignment1.py
+++ robot-sim/assignment1.py
@@ -2,19 +2,6 @@ from __future__ import print_function
 import time
 from sr.robot import *
 
-"""
-Exercise 1 python script
-
-Put the main code after the definition of the functions. The code should drive the robot around the environment
-Steps to be performed:
-1- give a linear velocity (speed 50, time 2)
-2- give an angular velocity (speed 20, time 2)
-3- move the robot in circle -> hint: you should create a new function setting the velocities so as to have a linear velocity + an angular velocity (speed 30, time 5)
-
-When done, run with:
-	$ python run.py solutions/exercise1_solution.py
-"""
-
 R = Robot()
 """ instance of the class Robot"""
 a_th = 2.0
