f64a6b8c81ea4ed1b011fdabca073dd48765bfab
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2022-10-18 11:27:21 +0000

    everything updated

---
+++ robot-sim/assignment.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+import time
+from sr.robot import *
+
+R = Robot()
+
+
+while 1:
+    pass
--- robot-sim/sr/robot/arenas/__init__.py
+++ robot-sim/sr/robot/arenas/__init__.py
@@ -5,12 +5,14 @@ from .sunny_side_up_arena import SunnySideUpArena
 from .abc_arena import ABCArena
 from .caldera_arena import CalderaArena
 from .two_colours_arena import TwoColoursArena
+from .two_colours_assignment_arena import TwoColoursAssignmentArena
 
 __all__ = [
     'pirate_plunder_arena',
     'ctf_arena',
     'sunny_side_up_arena',
     'two_colours_arena',
+    'two_colours_assignment_arena',
     'abc_arena',
     'caldera_arena',
     'arena',
